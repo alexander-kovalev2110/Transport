5d6cf499cfb123bad14963f33041b2fe8f80d163
alexander-kovalev2110 <alexander.kovalev@yahoo.com>
2024-05-26 20:40:09 +0300

    tripcalc

--- src/TripCalculator.php
+++ src/TripCalculator.php
@@ -18,12 +18,12 @@ class TripCalculator
         $this->vehicles[] = $vehicle;
     }
 
-    public function calculate($passengers, $baggageWeight, $distance, $fuelPrice, $kilometerRate)
+    public function calculate($passengers, $baggage, $distance, $fuelPrice, $kilometerRate)
     {
         $results = [];
         foreach ($this->vehicles as $vehicle) {
             try {
-                $cost = $vehicle->calculateTripCost($passengers, $baggageWeight, $distance, $fuelPrice, $kilometerRate);
+                $cost = $vehicle->calculateTripCost($passengers, $baggage, $distance, $fuelPrice, $kilometerRate);
                 if ($cost > 0) {
                     $results[] = ['vehicle' => $vehicle->getName(), 'cost' => $cost];
                 }
